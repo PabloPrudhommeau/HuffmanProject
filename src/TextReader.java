6f1bc7ee0907487a962d9cf44d66034fc139c1aa
kimypgm <ngn.julia@gmail.com>
2013-12-26 10:10:47 +0100

    Création de la classe qui analyse le texte

---
+++ src/TextReader.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Kimy
+ * Date: 26/12/13
+ * Time: 10:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class TextReader {
+}

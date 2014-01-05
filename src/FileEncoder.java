f2d38a381ff8e6d665952068ec13e323cdc9f296
sh444dow <pablo.prudhommeau@gmail.com>
2014-01-05 22:43:36 +0100

    ok2

--- src/FileEncoder.java
+++ src/FileEncoder.java
@@ -1,12 +1,11 @@
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class FileEncoder {
 
     private ArrayList<Node> tree;
     private String fileUrl;
-    private int[] frequencyArray = new int[256];
+    private int[] frequencyArrayLetters = new int[256];
 
     public FileEncoder(String url) {
         this.fileUrl = url;
@@ -20,27 +19,30 @@ public class FileEncoder {
         }
     }
 
+    public void sortFrequencyArrayLetters(){
+
+    }
+
     public void setLettersFrequency() throws IOException {
         InputStream inputFile = new FileInputStream(fileUrl);
 
-        int data = inputFile.read(), nbOctetsCompte = 0;
+        int data = inputFile.read();
+        int bytesCount = 0;
 
         while(data != -1) {
-            nbOctetsCompte++;
-            frequencyArray[data]++;
+            bytesCount++;
+            frequencyArrayLetters[data]++;
             data = inputFile.read();
         }
 
-        int nbCarac = 0;
+        int charNumber = 0;
         int i = 0;
         for( i = 0; i < 256 ; i++) {
-            if(frequencyArray[i] != 0){
-                System.out.println((char)i+" - "+frequencyArray[i]);
-                nbCarac++;
+            if(frequencyArrayLetters[i] != 0){
+                System.out.println((char)i+" - "+frequencyArrayLetters[i]);
+                charNumber++;
             }
-
         }
-
 
     }
 

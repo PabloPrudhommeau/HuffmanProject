60364bbccf90f829a6d74878cfe2bd2c7544fbb1
kimypgm <ngn.julia@gmail.com>
2014-01-05 23:44:04 +0100

    Merge Sort

--- src/FileEncoder.java
+++ src/FileEncoder.java
@@ -6,6 +6,7 @@ public class FileEncoder {
     private ArrayList<Node> tree;
     private String fileUrl;
     private int[] frequencyArrayLetters = new int[256];
+    private int[] arr2 = new int[256];
 
     public FileEncoder(String url) {
         this.fileUrl = url;
@@ -14,6 +15,15 @@ public class FileEncoder {
     public void encode() {
         try {
             this.setLettersFrequency();
+            this.mergeSort(0, 255);
+            System.out.println("Merge Sort");
+               int i =0;
+            for( i = 0; i < 256 ; i++) {
+                if(frequencyArrayLetters[i] != 0){
+                    System.out.println(frequencyArrayLetters[i]);
+                }
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -43,6 +53,35 @@ public class FileEncoder {
                 charNumber++;
             }
         }
+
+    }
+
+    public void mergeSort(int low, int high) {
+        if (low < high) {
+            int mid = (low+high) / 2;
+            this.mergeSort(low,mid);
+            this.mergeSort(mid+1, high);
+            this.merge(low, mid, high);
+        }
+    }
+
+    public void merge(int low, int mid, int high) {
+        int h = low, i = low, j = mid+1, k;
+        while ((h <= mid) && (j <= high)) {
+            if (this.frequencyArrayLetters[h] <= this.frequencyArrayLetters[j]) {
+                arr2[i] = this.frequencyArrayLetters[h]; h++;
+            }
+            else {
+                arr2[i] = this.frequencyArrayLetters[j]; j++;
+            } i++;
+        }
+        if (h > mid) for (k=j; k<=high; k++) {
+            arr2[i] = this.frequencyArrayLetters[k]; i++;
+        }
+        else for (k=h; k<=mid; k++) {
+            arr2[i] = this.frequencyArrayLetters[k]; i++;
+        }
+        for (k=low; k<=high; k++) this.frequencyArrayLetters[k] = arr2[k];
 
     }
 

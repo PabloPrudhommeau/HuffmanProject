f9cac42ba515210c1274c14c941c66ec4c9ff95d
sh444dow <pablo.prudhommeau@gmail.com>
2014-01-06 20:43:35 +0100

    Ajout d'une interface parcourir

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,72 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
-public class Main {
+public class Main extends JFrame {
+
+    private String selectedFile;
+    private String selectedFolder;
 
     public Main() {
-        FileEncoder encoder = new FileEncoder("res/Les_miserables_Tome_I_Chapitre_1-5.txt");
-        encoder.encode();
-        encoder.write("res/file[zipped].txt");
 
-        FileDecoder decoder = new FileDecoder(encoder.getBinaryTable(), "res/file[zipped].txt");
-        decoder.decode();
-        decoder.write("res/file[unzipped].txt");
+        this.setVisible(true);
+        this.setSize(400, 300);
+        this.setLayout(new GridBagLayout());
+
+        JButton buttonFileCompressed = new JButton("Choix du fichier à compresser");
+        buttonFileCompressed.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser();
+                chooser.setApproveButtonText("Choix du fichier à compresser");
+                if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+                    selectedFile = chooser.getSelectedFile().getAbsolutePath();
+                }
+            }
+        });
+
+        JButton buttonFolderOut = new JButton("Choix du répertoire de sortie");
+        buttonFolderOut.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser chooser = new JFileChooser();
+                chooser.setApproveButtonText("Choix du répertoire de sortie");
+                chooser.setFileSelectionMode((JFileChooser.DIRECTORIES_ONLY));
+                if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+                    selectedFolder = chooser.getSelectedFile().getAbsolutePath();
+                }
+            }
+        });
+
+        JButton buttonLaunchCompression = new JButton("Lancer la compression");
+        buttonLaunchCompression.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                FileEncoder encoder = new FileEncoder(selectedFile);
+                encoder.encode();
+                encoder.write(selectedFile + ".packed");
+
+                FileDecoder decoder = new FileDecoder(encoder.getBinaryTable(), selectedFile+".packed");
+                decoder.decode();
+                decoder.write(selectedFile+".unpacked");
+            }
+        });
+
+        this.add(buttonFileCompressed);
+        this.add(buttonFolderOut);
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.ipady = 0;       //reset to default
+        c.weighty = 1.0;   //request any extra vertical space
+        c.anchor = GridBagConstraints.PAGE_END; //bottom of space
+        c.insets = new Insets(10,0,0,0);  //top padding
+        c.gridx = 0;       //aligned with button 2
+        c.gridwidth = 3;   //2 columns wide
+        c.gridy = 0;       //third row
+        this.getContentPane().add(buttonLaunchCompression, c);
     }
 
     public static void main(String[] args) {
